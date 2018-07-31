23a33defdbe0d7a4fd4ce897387f6ef50ef2feef
James PS <jamesarems@gmail.com>
2018-07-31 11:24:45 +0400

    Update Stable 0.2

--- login.sh
+++ login.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 ##SSH Login notify with two factor auth]\
  LOCATION=`pwd`
+ LOG='/var/log'
+ TIME=`date +"%Y_%m_%d_%I_%M_%p"`
  echo $RANDOM > $LOCATION/code.txt
  CODE=`cat $LOCATION/code.txt`
  echo "Linux Server OTP: $CODE " | mail -s "Server OTP" test@test.com
  echo "A mail has been send to your admin"
  read -p "Please type the code:" b
 if [ "$b" == "$CODE" ]; then
-echo "Login Success"
+ echo "Login Success"
+ mv $LOCATION/code.txt $LOG/sln-code-$TIME.txt
 else
-echo "This is very secure server, we are logging out you from server"
-logout
-fi
+ echo "This is very secure server, we are logging out you from server"
+ logout
 fi
